8a7ee0cdf10c9a85a79c89b6cac09157e73d63cf
Luca Walker <77lucawalker@gmail.com>
2025-09-24 22:15:36 -0700

    updates

---
+++ script.js
@@ -0,0 +1,8 @@
+// Show a popup when page loads
+//alert("Welcome to my website!");
+
+// Function to change text
+function changeMessage() {
+  document.getElementById("message").innerText = "You clicked the button!";
+}
+
